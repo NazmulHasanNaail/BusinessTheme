6269edcb2f0b226c39dfa89cec4844233a8faf76
Nazmul <nazmulhasanfci@gmail.com>
2021-06-04 18:12:07 +0600

    Navigation and Custom Logo Upload

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php bloginfo('name'); ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_url'); ?>/css/font-awesome.css" rel="stylesheet">
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
+      <div class="header clearfix">
+        <div class="container">
+          <?php
+          wp_nav_menu( array(
+            'theme_location'  => 'primary',
+            'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+            'container'       => false,
+            'menu_class'      => 'nav nav-pills pull-right',
+            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'          => new WP_Bootstrap_Navwalker(),
+          ) );
+          ?>
+
+          <?php if(has_custom_logo()): ?>
+          <?php the_custom_logo(); ?>
+          <?php else: ?>
+          <h1><?php bloginfo('name'); ?></h1>
+          <?php endif; ?>
+
+        </div>
+      </div>
