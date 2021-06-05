28edda8c8b4ea01248bfd547ee95fa6a2c137e53
Nazmul <nazmulhasanfci@gmail.com>
2021-06-06 00:21:24 +0600

    Theme widgets Position

--- functions.php
+++ functions.php
@@ -11,5 +11,50 @@ require_once('class-wp-bootstrap-navwalker.php');
  }
  add_action('after_setup_theme', 'business_theme_setup');
 
+ //widgets init
+ function init_widgets(){
+     register_sidebar(array(
+         'name' => 'Content Region 1',
+         'id'   => 'content_region_1',
+         'before_widget' => ' <section class="row content-region-1 pt50 pb40"><div class="container"><div class="col-md-12">',
+         'after_widget' => '</div></div></section>',
+         'before_title' => '<h1>',
+         'after_title' => '</h1>'
+     ));
+     register_sidebar(array(
+        'name' => 'Content Region 2',
+        'id'   => 'content_region_2',
+        'before_widget' => '<section class="row content-region-2 pt40 pb40"><div class="container">',
+        'after_widget' => '</div></section>',
+        'before_title' => '<h1>',
+        'after_title' => '</h1>'
+    ));
+    register_sidebar(array(
+        'name' => 'Footer 1',
+        'id'   => 'footer_1',
+        'before_widget' => '<div class="col-lg-4">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>'
+    ));
+    register_sidebar(array(
+        'name' => 'Footer 2',
+        'id'   => 'footer_2',
+        'before_widget' => '<div class="col-lg-4">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>'
+    ));
+    register_sidebar(array(
+        'name' => 'Footer 3',
+        'id'   => 'footer_3',
+        'before_widget' => '<div class="col-lg-4">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>'
+    ));
+ }
+ add_action('widgets_init', 'init_widgets');
+
 //Customizer File
 require get_template_directory().'/inc/customizer.php';
